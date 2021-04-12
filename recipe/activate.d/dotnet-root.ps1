9524bc3d6ccb747e65e2e9d26bee84aeb1d818e2
David Hirschfeld <david.hirschfeld@stanwell.com>
2021-04-12 21:16:41 +1000

    Fix environment variable names

--- recipe/activate.d/dotnet-root.ps1
+++ recipe/activate.d/dotnet-root.ps1
@@ -4,8 +4,8 @@ $env:_CONDA_BACKUP_DOTNET_TOOLS = $env:DOTNET_TOOLS
 $env:DOTNET_ROOT = Join-Path $env:CONDA_PREFIX dotnet
 $env:DOTNET_TOOLS = Join-Path $env:DOTNET_ROOT tools
 
-$env:DOTNET_TRY_CLI_TELEMETRY_OPTOUT='true'
-$env:DOTNET_TRY_SKIP_FIRST_TIME_EXPERIENCE='true'
+$env:DOTNET_CLI_TELEMETRY_OPTOUT='true'
+$env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE='true'
 $env:DOTNET_ADD_GLOBAL_TOOLS_TO_PATH='false'
 
 # remove existing entries to prevent duplicates
