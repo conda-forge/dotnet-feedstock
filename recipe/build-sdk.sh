38d386b96f5d4f28d3fbdbba452ced347a92fdd4
acesnik <anthony.cesnik@gmail.com>
2021-04-14 19:02:04 -0500

    no packs/ or templates/ in 2.1

--- recipe/build-sdk.sh
+++ recipe/build-sdk.sh
@@ -12,6 +12,4 @@ else
 fi
 
 mkdir -p "${DOTNET_ROOT}"
-cp -r ./dotnet/packs/ "${DOTNET_ROOT}/packs/"
 cp -r ./dotnet/sdk/ "${DOTNET_ROOT}/sdk/"
-cp -r ./dotnet/templates/ "${DOTNET_ROOT}/templates/"
