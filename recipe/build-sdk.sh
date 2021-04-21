48445a452ee12d12a6570ea9d96c88647e2d2f93
acesnik <anthony.cesnik@gmail.com>
2021-04-20 19:31:56 -0500

    dotnet-runtime test; fix environment variables for macos

--- recipe/build-sdk.sh
+++ recipe/build-sdk.sh
@@ -3,12 +3,10 @@ set -eox pipefail
 
 PREFIX=$(echo "${PREFIX}" | tr '\\' '/')
 
-if [[ "${build_platform}" == "win-64" ]]; then
+if [[ "${target_platform}" == "win-64" ]]; then
     DOTNET_ROOT="${PREFIX}/dotnet"
 else
     DOTNET_ROOT="${PREFIX}/lib/dotnet"
-    cp $RECIPE_DIR/dotnet.bash $PREFIX/bin/dotnet
-    chmod +x $PREFIX/bin/dotnet
 fi
 
 mkdir -p "${DOTNET_ROOT}"
