21a9ec005d953a1a3bb8372c18bcd7a3c24e2428
acesnik <anthony.cesnik@gmail.com>
2021-07-06 16:35:42 +0200

    try setting them as environment variables?

--- recipe/build-sdk.sh
+++ recipe/build-sdk.sh
@@ -7,7 +7,6 @@ if [[ "${target_platform}" == "win-64" ]]; then
     DOTNET_ROOT="${PREFIX}/dotnet"
 else
     DOTNET_ROOT="${PREFIX}/lib/dotnet"
-    LD_LIBRARY_PATH="${PREFIX}/lib" # https://github.com/dotnet/core/blob/main/Documentation/build-and-install-rhel6-prerequisites.md#troubleshooting
 fi
 
 mkdir -p "${DOTNET_ROOT}"
