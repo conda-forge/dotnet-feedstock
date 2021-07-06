2d9df7d858f6fc239a60115e7520474b92ca3113
acesnik <anthony.cesnik@gmail.com>
2021-07-06 16:05:46 +0200

    try to fix ICU build issue

--- recipe/build-sdk.sh
+++ recipe/build-sdk.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -eox pipefail
 
+LD_LIBRARY_PATH="/usr/local/lib" # https://github.com/dotnet/core/blob/main/Documentation/build-and-install-rhel6-prerequisites.md#troubleshooting
 PREFIX=$(echo "${PREFIX}" | tr '\\' '/')
 
 if [[ "${target_platform}" == "win-64" ]]; then
