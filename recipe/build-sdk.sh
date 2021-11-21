0c2721b088e69decef8b8bf43c3d408786a7f028
Benedikt Reinartz <filmor@gmail.com>
2021-11-21 15:41:32 +0100

    Copy sdk-manifests that are required for .NET 6

--- recipe/build-sdk.sh
+++ recipe/build-sdk.sh
@@ -12,4 +12,7 @@ fi
 mkdir -p "${DOTNET_ROOT}"
 cp -r ./dotnet/packs/ "${DOTNET_ROOT}/packs/"
 cp -r ./dotnet/sdk/ "${DOTNET_ROOT}/sdk/"
+if [[ -e ./dotnet/sdk-manifests/ ]]; then
+    cp -r ./dotnet/sdk-manifests/ "${DOTNET_ROOT}/sdk-manifests/"
+fi
 cp -r ./dotnet/templates/ "${DOTNET_ROOT}/templates/"
