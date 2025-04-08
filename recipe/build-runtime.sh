a3756ec99c44dcd989eb34817bec56c6f2b5a915
memento <claude.rc@gmail.com>
2025-04-08 11:29:05 -0500

    (fix) wrong version of ttng-ust

--- recipe/build-runtime.sh
+++ recipe/build-runtime.sh
@@ -16,6 +16,11 @@ cp ./dotnet/dotnet* "${DOTNET_ROOT}"
 cp -r ./dotnet/shared/Microsoft.NETCore.App/ "${DOTNET_ROOT}/shared/Microsoft.NETCore.App/"
 cp -r ./dotnet/host/ "${DOTNET_ROOT}/host/"
 
+if [[ "${target_platform}" == "linux-"* ]]; then
+    # Hack to fix liblttng-ust dependency issues. liblttng-ust.so.0 is no longer supported.
+    patchelf --remove-needed liblttng-ust.so.0 ${DOTNET_ROOT}/shared/Microsoft.NETCore.App/${DOTNET_RUNTIME_VERSION}/libcoreclrtraceptprovider.so
+fi
+
 mkdir -p "${PREFIX}/etc/conda/activate.d"
 mkdir -p "${PREFIX}/etc/conda/deactivate.d"
 cp -r "${RECIPE_DIR}/activate.d/." "${PREFIX}/etc/conda/activate.d/"
