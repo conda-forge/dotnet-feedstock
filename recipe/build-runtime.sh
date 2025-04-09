de4d07cfb418b421ef09979dfad22d6c6a38bebb
memento <claude.rc@gmail.com>
2025-04-08 20:41:10 -0500

    (fix) drastic

--- recipe/build-runtime.sh
+++ recipe/build-runtime.sh
@@ -20,6 +20,13 @@ if [[ "${target_platform}" == "linux-"* ]]; then
     # Hack to fix liblttng-ust dependency issues. liblttng-ust.so.0 is no longer supported.
     patchelf --remove-needed liblttng-ust.so.0 ${DOTNET_ROOT}/shared/Microsoft.NETCore.App/${DOTNET_RUNTIME_VERSION}/libcoreclrtraceptprovider.so
 fi
+if [[ "${target_platform}" == "linux-aarch64" ]]; then
+  patchelf --remove-needed ld-linux-x86-64.so.2 ${DOTNET_ROOT}/dotnet
+  patchelf --remove-needed ld-linux-x86-64.so.2 ${DOTNET_ROOT}/host/fxr/${DOTNET_RUNTIME_VERSION}/libhostfxr.so
+  for lib in libmscordaccore.so libcoreclr.so libhostpolicy.so libclrjit.so libmscordbi.so; do
+    patchelf --remove-needed ld-linux-x86-64.so.2 ${DOTNET_ROOT}/shared/Microsoft.NETCore.App/${DOTNET_RUNTIME_VERSION}/$lib
+  done
+fi
 
 mkdir -p "${PREFIX}/etc/conda/activate.d"
 mkdir -p "${PREFIX}/etc/conda/deactivate.d"
