615de5e8663d36d023083bf115fbc16c3df174da
memento <claude.rc@gmail.com>
2025-04-09 17:48:47 -0500

    (fix) aarch warning due to wrong upstream

--- recipe/build-sdk.sh
+++ recipe/build-sdk.sh
@@ -16,10 +16,3 @@ if [[ -e ./dotnet/sdk-manifests/ ]]; then
     cp -r ./dotnet/sdk-manifests/ "${DOTNET_ROOT}/sdk-manifests/"
 fi
 cp -r ./dotnet/templates/ "${DOTNET_ROOT}/templates/"
-
-if [[ "${target_platform}" == "linux-aarch64" ]]; then
-  patchelf --remove-needed ld-linux-x86-64.so.2 ${DOTNET_ROOT}/sdk/9.0.202/AppHostTemplate/apphost
-  for lib in apphost libnethost.so singlefilehost; do
-    patchelf --remove-needed ld-linux-x86-64.so.2 ${DOTNET_ROOT}/packs/Microsoft.NETCore.App.Host.linux-x64/${DOTNET_RUNTIME_VERSION}/runtimes/linux-x64/native/$lib
-  done
-fi
